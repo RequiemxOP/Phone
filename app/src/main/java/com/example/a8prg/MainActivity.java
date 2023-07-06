d36be4536a0f4e9d3d8c80c3082d48dc2e3f3567
Lenovo <Lenovo@LAPTOP-CRQEM9PA>
2023-07-06 23:50:51 +0530

    Commit Final

---
+++ app/src/main/java/com/example/a8prg/MainActivity.java
@@ -0,0 +1,59 @@
+package com.example.a8prg;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+    EditText Edittext;
+    Button clearBtn,callBtn,saveBtn;
+
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Edittext=findViewById(R.id.Edittext);
+        callBtn=findViewById(R.id.callBtn);
+        saveBtn=findViewById(R.id.saveBtn);
+        clearBtn=findViewById(R.id.clearBtn);
+        clearBtn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Edittext.setText("");
+            }
+        });
+        callBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+
+            public void onClick(View v) {
+                String phoneNumber=Edittext.getText().toString();
+                Intent intent=new Intent(Intent.ACTION_DIAL);
+                intent.setData(Uri.parse("tel:"+phoneNumber));
+                startActivity(intent);
+            }
+        });
+        saveBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String phoneNumber=Edittext.getText().toString();
+                Intent intent=new Intent(Intent.ACTION_INSERT);
+                intent.setType(ContactsContract.Contacts.CONTENT_TYPE);
+                intent.putExtra(ContactsContract.Intents.Insert.PHONE,phoneNumber);
+                startActivity(intent);
+            }
+        });
+    }
+    public void inputNumber(View V){Button btn=(Button)V;
+        String digit=btn.getText().toString();
+        String phoneNumber=Edittext.getText().toString();
+        Edittext.setText (phoneNumber +digit);
+    }
+}
